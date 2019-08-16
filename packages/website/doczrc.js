979ccb3acc270d7117bebebfb400dd6f7928fd62
Zhang Yuxiang <zhangyuxiang@growingio.com>
2019-08-16 18:57:08 +0800

    feat: add icon

--- packages/website/doczrc.js
+++ packages/website/doczrc.js
@@ -3,13 +3,14 @@ import { css } from 'docz-plugin-css';
 module.exports = {
   title: 'GrowingIO Design System',
   typescript: true,
-  src: '..',
+  src: './src',
   menu: [
     'Overview',
     'Getting Started',
     'Design Language',
     'Design Tokens',
     'Components',
+    'Icons',
     'Changelog'
   ],
   plugins: [

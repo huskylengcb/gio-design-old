88ae5894c605bde56b19f71b6c68b8c7092889f3
Zhang Yuxiang <zhangyuxiang@growingio.com>
2019-08-22 19:05:45 +0800

    fix: theme d.ts

--- packages/playground/src/Hello.tsx
+++ packages/playground/src/Hello.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { colors } from '@gio-design/tokens';
 import Icon from '@gio-design/icon';
+import { indigo } from '@gio-design/themes';
 
 const Hello = () => {
   return (
-    <div style={{ color: colors.colorPrimary }}>
+    <div style={{}}>
       <Icon type='wechat' />
       Hello Parcel
     </div>

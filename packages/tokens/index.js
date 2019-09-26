80f079b1ca2912735142e8117c76c746747b36e4
Sihan Zhao <zhaosihan@growingio.com>
2019-09-26 18:40:21 +0800

    fix token require

--- packages/tokens/index.js
+++ packages/tokens/index.js
@@ -1,9 +1,12 @@
 const backgroundColors = require('./dist/backgroundColors.common.js');
 const borderColors = require('./dist/borderColors.common.js');
 const colors = require('./dist/colors.common.js');
-const fontSize = require('./dist/fontSize.common.js');
-const radius = require('./dist/radius.common.js');
-const shadow = require('./dist/shadow.common.js');
+// const fontSize = require('./dist/fontSize.common.js');
+// const radius = require('./dist/radius.common.js');
+// const shadow = require('./dist/shadow.common.js');
+const fontSize = null;
+const radius = null;
+const shadow = null;
 const textColors = require('./dist/textColors.common.js');
 
 module.exports = {

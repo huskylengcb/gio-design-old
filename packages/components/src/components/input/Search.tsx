0b3a2efc37437fcb24ad6ce1874e5dbfac279c54
chen_gh <chenguanghao@growingio.com>
2019-10-18 15:39:25 +0800

    update search input

--- packages/components/src/components/input/Search.tsx
+++ packages/components/src/components/input/Search.tsx
@@ -1,7 +1,6 @@
 import React, { ChangeEvent } from 'react';
 import { Input } from 'antd';
 import { SearchProps } from 'antd/lib/input';
-import { debounce } from 'lodash';
 
 const SearchInput = Input.Search;
 
@@ -10,19 +9,16 @@ export default class Search extends React.Component<SearchProps> {
     onSearch: (v: string) => void 0
   }
 
-  public debouncedSearch = debounce(this.props.onSearch, 300)
-
-  public handleSearch = (e: ChangeEvent<HTMLInputElement>) =>
-    this.debouncedSearch(e.target.value);
+  public handleSearch = (e: ChangeEvent<HTMLInputElement>) => this.props.onSearch(e.target.value);
 
   public render() {
     return (
     <SearchInput
+      size='large'
       {...this.props}
       onChange={this.handleSearch}
       onSearch={void 0}
       className={`gio-input-search ${this.props.className}`}
-      size='large'
     />
     )
   }
